eec1f129c16ae60aff1e432b3e9bcb55fc234b6f
yrivero <yermir@outlook.com>
2017-03-12 16:32:54 +0100

    Implementation of the single digit case

--- src/main/java/com/hexad/katasyermirsergio/numbersinwords/NumberInWords.java
+++ src/main/java/com/hexad/katasyermirsergio/numbersinwords/NumberInWords.java
@@ -34,7 +34,7 @@ public class NumberInWords {
     }
 
     private String formatDollarsSuffix(double plainNumber) {
-        if (plainNumber == 1.0D) {
+        if (plainNumber == 1.0D ) {
             return "dollar";
         }
         else {
@@ -46,12 +46,26 @@ public class NumberInWords {
         Double number = new Double(plainNumber);
 
         switch (number.intValue()) {
+            case 0:
+                return "zero";
             case 1:
                 return "one";
             case 2:
                 return "two";
             case 3:
                 return "three";
+            case 4:
+                return "four";
+            case 5:
+                return "five";
+            case 6:
+                return "six";
+            case 7:
+                return "seven";
+            case 8:
+                return "eight";
+            case 9:
+                return "nine";
             default:
                 throw new UnsupportedOperationException("Not implemented yet!");
         }
