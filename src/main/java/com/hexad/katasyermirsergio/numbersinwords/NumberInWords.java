c3a4ef76b503c5e4f167fedbeec7fbbc5d2aa5d5
yrivero <yermir@outlook.com>
2017-03-12 13:27:07 +0100

    Failed test givenNumber10ShouldReturnTenDollars

--- src/main/java/com/hexad/katasyermirsergio/numbersinwords/NumberInWords.java
+++ src/main/java/com/hexad/katasyermirsergio/numbersinwords/NumberInWords.java
@@ -6,13 +6,28 @@ package com.hexad.katasyermirsergio.numbersinwords;
 public class NumberInWords {
 
     public String convertNumber(String textOfNumber) {
+        double plainNumber = extractPlainNumber(textOfNumber);
+        String formattedNumber = formatNumber(plainNumber);
+
+        return formattedNumber;
+    }
+
+    private double extractPlainNumber(String textOfNumber) {
         if (textOfNumber.equals("1$")) {
+            return 1.0D;
+        }
+        else {
+            return 2.0D;
+        }
+    }
+
+    private String formatNumber(Double plainNumber) {
+        if (plainNumber == 1.0D) {
             return "one dollar";
         }
         else {
             return "two dollars";
         }
-
     }
 
 }
