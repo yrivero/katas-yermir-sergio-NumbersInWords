7e4757d9b616157412ef7297c4870ec8f17b3196
yrivero <yermir@outlook.com>
2017-03-12 11:58:06 +0100

    Failing test givenNumberOneShouldReturnOneDollar

---
+++ src/main/java/com/hexad/katasyermirsergio/numbersinwords/NumberInWords.java
@@ -0,0 +1,12 @@
+package com.hexad.katasyermirsergio.numbersinwords;
+
+/**
+ * Created by Hexad GmbH on 12/03/2017.
+ */
+public class NumberInWords {
+
+
+    public String convertNumber(String textOfNumber) {
+        return null;
+    }
+}
--- src/test/java/com/hexad/katasyermirsergio/numbersinwords/NumbersInWordsTest.java
+++ src/test/java/com/hexad/katasyermirsergio/numbersinwords/NumbersInWordsTest.java
@@ -1,7 +1,17 @@
 package com.hexad.katasyermirsergio.numbersinwords;
 
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 /**
  * Created by Hexad GmbH on 12/03/2017.
  */
 public class NumbersInWordsTest {
+
+    @Test
+    public void givenNumberOneShouldReturnOneDollar() throws Exception {
+        NumberInWords numberInWords = new NumberInWords();
+        String wordsOfTheNumber = numberInWords.convertNumber("1$");
+        assertEquals("one dollar", wordsOfTheNumber);
+    }
 }
